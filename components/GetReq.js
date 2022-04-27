52cf5dead9f0d3e066f7fee733a249d6a5cd71c0
PDuff234 <pduffany@mail.usf.edu>
2022-04-27 17:18:38 -0400

    Merged Chantals Changes

--- components/GetReq.js
+++ components/GetReq.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const ListingData = () => {
   const [drinks, setDrinks] = useState([]);
@@ -17,6 +18,36 @@ const ListingData = () => {
   useEffect(() => {
     getDrinks();
   }, []);
+
+  const navigation = useNavigation(); 
+  function orderDrink(drink, price){
+
+    var order = {
+      customer: sessionStorage.getItem("user"),
+      cocktail: drink,
+      total: price
+    };
+
+    console.log(order.cocktail);
+    console.log(order.total);
+    
+    fetch('http://localhost:5000/makeOrder', {
+      method: 'POST', 
+      headers: {
+        Accept: 'application/json', 
+        'Content-Type': 'application/json'
+      }, 
+      body: JSON.stringify(order)
+    })
+    .then(response => {
+      console.log("response: " + response);
+      if (response.status === 200) {
+        console.log("Response is 200")
+        console.log("Order has been made")
+        navigation.push('Customer Screen')
+      }
+    })
+  }
 
   console.log(drinks);
 
@@ -37,6 +68,11 @@ const ListingData = () => {
               <td>{drink.name}</td>
               <td>{drink.description}</td>
               <td>{drink.price}</td>
+              <td>
+              <button className="orderButton" onClick={() => orderDrink(drink.name, drink.price)}>
+                Order this Drink
+              </button>
+              </td>
             </tr>
           ))}
         </tbody>
--- screens/CustomerScreen.js
+++ screens/CustomerScreen.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { StyleSheet, View, ScrollView } from "react-native";
 import { Table, TableWrapper, Row } from 'react-native-table-component'; 
+import { useNavigation } from "@react-navigation/native";
 
 const CustomerHome = () => {
   const [drinks, setDrinks] = useState([]);
@@ -19,6 +20,36 @@ const CustomerHome = () => {
   useEffect(() => {
     getDrinks();
   }, []);
+
+  const navigation = useNavigation(); 
+  function orderDrink(drink, price){
+
+    var order = {
+      customer: sessionStorage.getItem("user"),
+      cocktail: drink,
+      total: price
+    };
+
+    console.log(order.cocktail);
+    console.log(order.total);
+    
+    fetch('http://localhost:5000/makeOrder', {
+      method: 'POST', 
+      headers: {
+        Accept: 'application/json', 
+        'Content-Type': 'application/json'
+      }, 
+      body: JSON.stringify(order)
+    })
+    .then(response => {
+      console.log("response: " + response);
+      if (response.status === 200) {
+        console.log("Response is 200")
+        console.log("Order has been made")
+        navigation.push('Customer Screen')
+      }
+    })
+  }
 
   console.log(drinks);
 
